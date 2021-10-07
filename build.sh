316016e6592f5728635966775e4a01b2fe3c9a57
Bren Briggs <code@fraq.io>
2021-10-07 13:32:52 -0400

    Basic build wrapper

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 set xe
-
-CLUSTER_NAME="hyperdrive"
-REGISTRY_NAME="hyperdrive"
+WORKDIR=$(pwd)
+CLUSTER_NAME="firefly"
+REGISTRY_NAME="firefly"
 REGISTRY_PORT="8081"
 SERVER_COUNT=1
 AGENT_COUNT=0
 
+# TODO: add check for k3d. Must be 4.4.8 or better.
 k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
 k3d cluster create ${CLUSTER_NAME} \
   --servers ${SERVER_COUNT} \
@@ -15,3 +16,10 @@ k3d cluster create ${CLUSTER_NAME} \
   --kubeconfig-update-default=true \
   --registry-use k3d-${REGISTRY_NAME}.localhost:${REGISTRY_PORT} \
   --k3s-server-arg '--no-deploy=traefik' \
+
+# TODO: add check for helm binary. Must be v3 or better.
+echo "Installing firefly stack..."
+
+helm install firefly ./helm/firefly
+
+cd $WORKDIR
