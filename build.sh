d328bab2f818f8d08ed93bdfe025c80b70633c84
Bren Briggs <code@fraq.io>
2021-10-07 18:54:29 -0400

    Notebooks are working locally now

--- build.sh
+++ build.sh
@@ -8,14 +8,16 @@ REGISTRY_PORT="8081"
 SERVER_COUNT=1
 AGENT_COUNT=0
 
-# TODO: add check for k3d. Must be 4.4.8 or better.
+# TODO: add check for k3d >= 5.0.0
+# k3s version must be 1.21.4 or better to avoid local-path permissions bug
 k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
 k3d cluster create ${CLUSTER_NAME} \
+  -p 8082:8081@server:0 \
   --servers ${SERVER_COUNT} \
   --agents ${AGENT_COUNT} \
   --kubeconfig-update-default=true \
   --registry-use k3d-${REGISTRY_NAME}.localhost:${REGISTRY_PORT} \
-  --k3s-server-arg '--no-deploy=traefik' \
+  --k3s-arg "--disable=traefik@server:0" \
 
 # TODO: add check for helm binary. Must be v3 or better.
 echo "Installing firefly stack..."
