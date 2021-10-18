12146cb651ba7d0b11620ee60d408b2b7a7421b0
csears-hg <chris.sears@hypergiant.com>
2021-10-18 11:52:37 -0400

    Initial commit of CloudFormation template and a couple related fixes

--- build.sh
+++ build.sh
@@ -11,13 +11,12 @@ FIREFLY_NAMESPACE="firefly"
 
 # TODO: add check for k3d >= 5.0.0
 # k3s version must be 1.21.4 or better to avoid local-path permissions bug
-k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
+#k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
 k3d cluster create ${CLUSTER_NAME} \
   -p 8090:80@loadbalancer \
   --servers ${SERVER_COUNT} \
   --agents ${AGENT_COUNT} \
   --kubeconfig-update-default=true \
-  --registry-use k3d-${REGISTRY_NAME}.localhost:${REGISTRY_PORT} \
   --k3s-arg "--disable=traefik@server:0"
 
 # TODO: add check for helm binary. Must be v3 or better.
@@ -25,3 +24,4 @@ echo "Installing firefly stack..."
 kubectl create namespace ${FIREFLY_NAMESPACE}
 helm install --namespace ${FIREFLY_NAMESPACE} firefly ./helm/firefly
 cd $WORKDIR
+echo "JupyterHub should be available at http://localhost:8090"
