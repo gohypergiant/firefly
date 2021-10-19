4a0a268b79e2e328b5003eb1ce8b9590fb1d8fd0
David Allen <david.allen@hypergiant.com>
2021-10-19 10:47:39 -0400

    change to yum

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
 echo "==> Updating list of repositories"
-apt-get clean
-apt-get -y update
+yum -y update
 
 echo "==> Installing docker"
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common lsb-release
+yum install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get -y update
-apt-get -y install docker-ce docker-ce-cli containerd.io
+yum -y update
+yum -y install docker-ce docker-ce-cli containerd.io
 usermod -aG docker ${SSH_USERNAME}
 
 echo "==> Adding docker to systemd"
