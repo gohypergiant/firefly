ee1c44c8957d6805387ec706fb71c5ac13a93d50
David Allen <david.allen@hypergiant.com>
2021-10-19 11:09:55 -0400

    adjustment to pin

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ REGISTRY_PORT="8081"
 SERVER_COUNT=1
 AGENT_COUNT=0
 FIREFLY_NAMESPACE="firefly"
-KUBE_VERSION="v1.21.0"
+IMAGE_VERSION="rancher/k3s:v1.17.13-k3s2"
 
 # TODO: add check for k3d >= 5.0.0
 # k3s version must be 1.21.4 or better to avoid local-path permissions bug
 k3d registry create ${REGISTRY_NAME}.localhost -p ${REGISTRY_PORT} ;
 k3d cluster create ${CLUSTER_NAME} \
   -p 8090:80@loadbalancer \
-  --version ${KUBE_VERSION} \
+  --image ${IMAGE_VERSION} \
   --servers ${SERVER_COUNT} \
   --agents ${AGENT_COUNT} \
   --kubeconfig-update-default=true \
